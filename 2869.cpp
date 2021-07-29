fea707750161a24ce31e511b4a81fc8f52a01a15
Sungyoun-Kim <ksungyoun@naver.com>
2021-07-30 02:13:15 +0900

    for문으로 돌리면 시간제한걸림, 복잡도를 낮춰서 성공함

---
+++ 2869.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    double a;
+    double b;
+    double v;
+
+    cin>>a>>b>>v;
+    double result=(v-b)/(a-b);
+    int result2=((int)v-(int)b)/((int)a-(int)b);
+    if(result>result2)
+    result2=result2+1;
+    
+    cout<<result2;
+}
